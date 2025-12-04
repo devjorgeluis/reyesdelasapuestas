877190ebaf526f83badf0c240f97988641784098
devjorgeluis <jorgencnc0608@gmail.com>
2025-12-04 15:35:40 +0800

    remove unused files

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,27 +1,14 @@
-import { useContext } from "react";
-import { LayoutContext } from "./Layout/LayoutContext";
-
-import IconFavorite from "/src/assets/svg/favorites.svg";
-
 const SearchInput = ({
     txtSearch,
     setTxtSearch,
     searchRef,
-    search,
-    isMobile
+    search
 }) => {
-    const { setShowMobileSearch } = useContext(LayoutContext);
 
     const handleChange = (event) => {
         const value = event.target.value;
         setTxtSearch(value);
         search({ target: { value }, key: event.key, keyCode: event.keyCode });
-    };
-
-    const handleFocus = () => {
-        if (isMobile) {
-            setShowMobileSearch(true);
-        }
     };
 
     return (
@@ -31,7 +18,6 @@ const SearchInput = ({
             value={txtSearch}
             onChange={handleChange}
             onKeyUp={search}
-            onFocus={handleFocus}
         />
     );
 };
